86cfa3830ab0c70f16b32717518a1be55e200220
Hamad Alvi <hamadalvi.dev@gmail.com>
2026-07-13 23:05:28 +0500

    t

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -34,7 +34,7 @@ const buttonVariants = cva(
         "icon-lg": "size-10",
       },
     },
-    //test 1111
+    //test 111
     defaultVariants: {
       variant: "default",
       size: "default",
